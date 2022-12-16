5de96ee8b2f7c90d552c3a4acf37a592bbf0a168
u1243865 <u1243865@ml-staff-av-pt-mac.staff.library.utah.edu>
2022-12-16 10:48:00 -0700

    Formatted and organized code, added minor improvements to frame location and resizing.

--- first-game/src/GUI/Drawing.java
+++ first-game/src/GUI/Drawing.java
@@ -1,46 +1,52 @@
 package GUI;
 
 import java.awt.Canvas;
-import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-
 import javax.imageio.ImageIO;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
 
+/**
+ * The drawing class for the octopus game.
+ * 
+ * @author Austin LaMontagne
+ * @version 1.0, December 15, 2022
+ */
+@SuppressWarnings("serial")
 public class Drawing extends Canvas{
 	
-	int i = 0;
-	BufferedImage img1 = null;
-	BufferedImage img2 = null;
+	int frame = 0;
+	BufferedImage octFrame1;
+	BufferedImage octFrame2;
 	
+	/**
+	 * Constructor for Drawing object. Reads image files.
+	 */
 	public Drawing() {
+		// Read Image Files to objects to be drawn.
 		try {
-		    img1 = ImageIO.read(new File("src/octopus1.png"));
-		    img2 = ImageIO.read(new File("src/octopus2.png"));
+		    octFrame1 = ImageIO.read(new File("src/octopus1.png"));
+		    octFrame2 = ImageIO.read(new File("src/octopus2.png"));
 		} catch (IOException e) {
 		}
 	}
 
-	@Override
-	public void paint(Graphics g) {
+	/**
+	 * The overridden paint method from Canvas to draw on our Drawing object.
+	 * 
+	 * @param g : Graphics object passed.
+	 */
+	public void paint(Graphics g) {	
 		
-		//g.drawImage(img, 100, 100, 200, 200, getParent());
-		
-		//g.setColor(Color.BLUE);
-		//g.fillOval(100, 100, 200, 200);	
-		
-		if(i%2 == 0) {
-			//g.fillRect(0, 0, 100, 100);
-			g.drawImage(img1, 72, 72, 256, 256, getParent());
+		// Draw and update octopus each frame per click.
+		if(frame % 2 == 0) {
+			g.drawImage(octFrame1, 72, 72, 256, 256, getParent());
 		}  
 		else {
-			g.drawImage(img2, 72, 72, 256, 256, getParent());
+			g.drawImage(octFrame2, 72, 72, 256, 256, getParent());
 		}
 		
-		i++;
+		frame++;
 	}
 }
--- first-game/src/GUI/clickerLayout.java
+++ first-game/src/GUI/clickerLayout.java
@@ -2,123 +2,137 @@ package GUI;
 
 import java.awt.BorderLayout;
 import java.awt.Canvas;
-import java.awt.Color;
-import java.awt.Graphics;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
-import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-
 import javax.imageio.ImageIO;
-import javax.swing.BorderFactory;
-import javax.swing.BoxLayout;
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.SpringLayout;
 
-// Test Comment To See if I can push from home.
-
+/**
+ * The layout and main method of the Octopus clicker game.
+ * 
+ * @author Austin LaMontagne
+ * @version 1.0, December 15, 2022
+ */
 public class clickerLayout implements ActionListener{
 	
+	// Declare variables and objects.
 	int count = 0;
+	int removeCongrats = 0;
+	int nextGoal = 0;
+	static int goalSpacing = 25;
+	boolean congratsBool = false;
 	JFrame frame;
 	JPanel panel;
-	JButton button;
-	JLabel label;
+	JButton clickButton;
+	JLabel noClicksLabel;
+	JLabel responseLabel;
 	Canvas canvas;
 	GridBagConstraints c;
 	
-	public clickerLayout() throws IOException{
+	/**
+	 * Constructor for clickerLayout object. Creates framing of game.
+	 */
+	public clickerLayout(){
 		
+		// Initialize objects.
 		frame = new JFrame("Octopus Game");
 		panel = new JPanel();
-		button = new JButton("Click me");
-		label = new JLabel("Number of clicks: 0");
+		clickButton = new JButton("Click me");
+		clickButton.addActionListener(this);	
+		noClicksLabel = new JLabel("Number of clicks: 0");
+		responseLabel = new JLabel("Octopus Game: Click to win!");
 		canvas = new Drawing();
 		canvas.setSize(400, 400);
 		
-		/*
-		//String imagePath = "/Users/u1243865/git/first-game/first-game/src/GUI/icon.png";
-		//BufferedImage myPicture = ImageIO.read(new File (imagePath));
-		ImageIcon img = loadIcon("src/image.png");
-		frame.setIconImage(img.getImage());
-		*/
+		// Read image and set icon.
 		BufferedImage img = null;
 		try {
 			img = ImageIO.read(new File("src/octopus1.png"));
 		} catch (IOException e) {
 		}
-		frame.setIconImage(img);
-		
-		button.addActionListener(this);		
-		
-		//GridLayout layout = new GridLayout(0, 2);
-		//SpringLayout layout = new SpringLayout();
+		String os = System.getProperty("os.name");
+		if(os.equals("Windows 10") || os.equals("Windows 11")) {
+			frame.setIconImage(img);
+		}
+		else if(os.equals("Mac OS X")) {
+			//Application.getApplication().setDockIconImage(img);
+		}
+				
+		// Set layout of panel.
 		GridBagLayout layout = new GridBagLayout();
 		panel.setLayout(layout);
 		
-		
+		// Add components with constraints to panel.
 		c = new GridBagConstraints();	
 		c.gridx = 1;
-		c.gridy = 1;
+		c.gridy = 2;
 		c.ipady = 40;
 		c.gridwidth = 2;
-		panel.add(button, c);
+		panel.add(clickButton, c);
+		c.gridx = 1;
+		c.gridy = 1;
+		c.ipady = 0;
+		panel.add(noClicksLabel, c);	
 		c.gridx = 1;
 		c.gridy = 0;
-		c.ipady = 0;
-		panel.add(label, c);		
+		panel.add(responseLabel, c);
 		c.gridwidth = 3;
 		c.gridx = 0;
-		c.gridy = 2;
+		c.gridy = 3;
 		panel.add(canvas, c);
 		
-		
-		//panel.add(button);
-		//panel.add(label);
-		//panel.add(canvas);
-		
-		// layout.putConstraint(SpringLayout.HORIZONTAL_CENTER, button, 5, SpringLayout.HORIZONTAL_CENTER, panel);
-		//layout.putConstraint(SpringLayout.EAST, button, 50 ,SpringLayout.HORIZONTAL_CENTER , panel);
-		//layout.putConstraint(SpringLayout.WEST, label, 50 ,SpringLayout.HORIZONTAL_CENTER , panel);
-		
-		//layout.putConstraint(SpringLayout.HORIZONTAL_CENTER, canvas, 10, SpringLayout.VERTICAL_CENTER, panel);
-	
-		
-		frame.add(panel, BorderLayout.CENTER);
-		frame.setBounds(0, 0, 1000, 1000);
-		
+		// Add panel to frame, set frame bounds, and set constraints.
+		frame.add(panel, BorderLayout.CENTER);	
 		frame.update(panel.getGraphics());
-		//frame.pack();
+		frame.pack();
+		frame.setLocationRelativeTo(null);
+        frame.setMinimumSize(frame.getSize()); 
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);	
 	}
 	
-	public static void main(String[] args) throws IOException {
+	/**
+	 * Main method to instantiate and run game.
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// Creates clickerLayout object to begin game.
 		new clickerLayout();
 	}
 	
-	/*
-	public ImageIcon loadIcon(String iconName) throws IOException {
-	    ClassLoader loader = this.getClass().getClassLoader();
-	    BufferedImage icon = ImageIO.read(loader.getResourceAsStream(iconName));
-	    return new ImageIcon(icon);
-	}
-	*/
-	
+	/**
+	 * Action performed method for clicking button.
+	 * 
+	 * @param e : ActionEvent from button clicked.
+	 */
 	public void actionPerformed(ActionEvent e) {
+		// Increments count and displays number of clicks.
 		count++;
-		label.setText("Number of clicks: " + count);
+		noClicksLabel.setText("Number of clicks: " + count);
+		
+		// Updates canvas to redraw images.
 		canvas.update(canvas.getGraphics());
+		
+		// Updates text to show congratulations and milestones messages.
+		if(count % goalSpacing == 0) {
+			responseLabel.setText("Congrats on reaching " + count + " clicks!");
+			congratsBool = true;
+			removeCongrats = 0;
+			nextGoal = count + goalSpacing;
+		}
+		else if(removeCongrats == 10 && congratsBool) {
+			responseLabel.setText("Next milestone: " + nextGoal + " clicks.");
+			congratsBool = false;
+		}
+		removeCongrats++;
 	}
-
 }
-
-
